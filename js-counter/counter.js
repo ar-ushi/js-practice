fca03661781ee43c30fc89afc721f08de06ec4f9
Arushi <aggarwalarushi5@gmail.com>
2023-11-07 01:11:09 +0530

    counter in js

--- js-counter/counter.js
+++ js-counter/counter.js
@@ -1,2 +1,22 @@
 //Two Implementations - Basic JS & JQuery
 
+var value = 0;
+document.addEventListener("DOMContentLoaded",function() {
+    const inputEle = document.getElementById('changeBy');
+    value = parseInt(inputEle.value);
+
+    inputEle.addEventListener('input', function(){
+        value = inputEle.value;
+    })
+});
+
+function updateValue(direction){
+    let currValue =  parseInt(document.getElementById('counter-val').innerHTML);
+    var updatedValue = currValue + (direction * value);
+    document.getElementById('counter-val').innerHTML = updatedValue;
+}
+
+function resetValue(){
+    document.getElementById('counter-val').innerHTML = 0;
+    document.getElementById('changeBy').value = 1;
+}
